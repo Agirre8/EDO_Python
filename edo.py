0255ba8ae6a1d9439e2adc870d2071818f5cabb8
Agirre8 <inagi30@gmail.com>
2022-10-06 14:33:08 +0200

    ecuacion resuelta

---
+++ edo.py
@@ -0,0 +1,15 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.integrate import odeint
+
+#y`+2y = e^(-3x)
+def dF(y,x):
+    dy = np.exp(-3*x)-2*y
+    return dy
+
+y0=1
+x = np.linspace(0,5,100)
+
+sol = odeint(dF,y0,x)
+
+
