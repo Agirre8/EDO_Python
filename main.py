8b5db2855c90f7f9f7f70e9581bba087d4770754
Agirre8 <inagi30@gmail.com>
2022-10-13 20:15:44 +0200

    main creado

---
+++ main.py
@@ -0,0 +1,9 @@
+from Ecuaciones import Ecuacion1 as Ec1
+from Ecuaciones import Ecuacion2 as Ec2
+from Ecuaciones import Ecuacion3 as Ec3
+from Ecuaciones import Ecuacion4 as Ec4
+
+Ec1.solucion()
+Ec2.solucion()
+Ec3.solucion()
+Ec4.solucion()
