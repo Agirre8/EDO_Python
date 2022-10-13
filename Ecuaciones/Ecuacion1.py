87ce06a86c54af1433c2b95cfbb7223343f09fbf
Agirre8 <inagi30@gmail.com>
2022-10-13 17:22:30 +0200

    update

--- Ecuaciones/Ecuacion1.py
+++ Ecuaciones/Ecuacion1.py
@@ -1,23 +1,21 @@
-#from Importaciones.importaciones import * 
-from cmath import exp, pi, sin, log
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.integrate import odeint
-import sympy
+
+from importaciones import *
+
 
 def dF(y,x):
-    dy = sympy(x**2*y-y)/(1+y)
+    dy = (x**2*y-y)/(1+y)
     return dy
+
 #Expresamos una ecuacion algebraica cualquiera
 
 y0=-1
-x0=3
-x = np.linspace(0,5,100)
 
-sol = odeint(dF,y0,x0,x)
+t = np.linspace(0,5)
+
+y = odeint(dF,y0,t)
 
 plt.figure()
-plt.plot(x,sol)
+plt.plot(t,y)
 plt.grid()
 plt.show()
 
--- Ecuaciones/importaciones.py
+++
@@ -1,5 +0,0 @@
-from cmath import exp, pi, sin, log
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.integrate import odeint
-import sympy
