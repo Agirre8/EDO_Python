0310db205747f0dc252248e1359227e8fafdc870
Agirre8 <inagi30@gmail.com>
2022-10-13 20:12:00 +0200

    funciones para resolver las ecuaciones

--- Ecuaciones/Ecuacion3.py
+++ Ecuaciones/Ecuacion3.py
@@ -1,13 +1,16 @@
 from Ecuacion1 import *
 
 
-def dF2(y,x,t):
+def dF2(y,t):
     dy2 = sympy(2*(t-2)**2*(y/(t-2)))
     return dy2
 
-sol = odeint(dF2,dF2.x)
+def solucion():
+    sol = odeint(dF2,t)
 
-plt.figure()
-plt.plot(dF2.x,sol)
-plt.grid()
-plt.show()
+    plt.figure()
+    plt.plot(t,sol)
+    plt.grid()
+    plt.show()
+
+solucion()
--- Ecuaciones/Ecuacion4.py
+++ Ecuaciones/Ecuacion4.py
@@ -1,14 +1,19 @@
+from turtle import end_fill
 from Ecuacion1 import *
 
 def dF3(y,t):
     dy3 = sympy(3*t**2+y)/(2*t)
     return dy3
 
-sol = odeint(dF3,t)
 
-plt.figure()
-plt.plot(t,sol)
-plt.grid()
-plt.show()
+def solucion(t):
+    sol = odeint(dF3,t)
+    plt.figure()
+    plt.plot(t,sol)
+    plt.grid()
+    plt.show()
+
+solucion()
+
 
 
